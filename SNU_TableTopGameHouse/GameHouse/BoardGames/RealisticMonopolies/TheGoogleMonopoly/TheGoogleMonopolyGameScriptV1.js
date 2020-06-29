894272b17c94c722caf61ebc68044cec9dae2e87
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-28 18:46:52 -0700

    5 game files
    5 game files for the Google Monopoly game, in HTML and JavaScript formats.

---
+++ SNU_TableTopGameHouse/GameHouse/BoardGames/RealisticMonopolies/TheGoogleMonopoly/TheGoogleMonopolyGameScriptV1.js
@@ -0,0 +1,28 @@
+//<script type="text/JavaScript">
+console.log("Cannot load this game, game is under development still and is far from ready");
+alert("Cannot play this file");
+// Revenue
+float startFund = 100000.00;
+float googleNetWorth = 100000.02;
+// Founders
+var founder1 = "Larry Page ";
+var founder2 = "Sergey Brin";
+// Time
+/* Note: This game follows the UNIX epoch time system (48 bit) for in-game time */
+int dateCurGame = 0;
+// Company starting
+function analyticalMachine1 {
+	
+}
+function developingGoogleSearch {
+	
+}
+// The Google search engine
+function launchGoogleSearch {
+	
+}
+/* File version history
+Version 1: January 28th 2020
+Version 2: Coming soon
+*/
+//</script>
---
+++ SNU_TableTopGameHouse/GameHouse/BoardGames/RealisticMonopolies/TheGoogleMonopoly/TheGoogleMonopolyGameScriptV3.js
@@ -0,0 +1,134 @@
+//<script type="text/JavaScript">
+console.log("Cannot load this game, game is under development still and is far from ready");
+alert("Cannot play this file");
+// Revenue
+float startFund = 100000.00;
+float googleNetWorth = 100000.02;
+// Founders
+var founder1 = "Larry Page ";
+var founder2 = "Sergey Brin";
+// Time
+/* Note: This game follows the UNIX epoch time system (48 bit) for in-game time */
+int dateCurGame = 0;
+// Teams
+function team1 { // The CEO and founders team
+	var team1_member1 == founder1; // Larry page (Replaces with Sundar Pichai in 2015)
+	var team1_member2 == founder2; // Sergey Brin
+	var team1_member3 = "CEO1";
+	var team1_member4 = "CEO2";
+	var team1_member5 = "CEO3";
+	var team1_member6 = "CEO4";
+	var team1_member7 = "CEO5";
+	var team1_member8 = "CEO6";
+	var team1_member9 = "CEO8";
+	var team1_member10 = "CEO8";
+	var team1_member11 = "CEO9";
+	var team1_member11 = "CEO10";
+}
+function team2 { // The Employee team
+	var team2_member1 == "Username.#1";
+	var team2_member2 == "Username #2";
+	var team2_member3 == "Username #3";
+	var team2_member4 == "Username.#4";
+	var team2_member5 == "Username #5";
+	var team2_member6 == "Username #6";
+	var team2_member7 == "Username.#7";
+	var team2_member8 == "Username #8";
+	var team2_member9 == "Username #9";
+	var team2_member10 == "Username.#10";
+	var team2_member11 == "Username #11";
+	var team2_member12 == "Username #12";
+}
+function team3 { // The consumer team
+	var team3_member1 == "Username #A1");
+	var team3_member2 == "Username #A2");
+	var team3_member3 == "Username #A3");
+	var team3_member4 == "Username #A4");
+	var team3_member5 == "Username #A5");
+	var team3_member6 == "Username #A6");
+	var team3_member7 == "Username #A7");
+	var team3_member8 == "Username #A8");
+	var team3_member9 == "Username #A9");
+	var team3_member10 == "Username #A10");
+	var team3_member11 == "Username #A11");
+	var team3_member12 == "Username #A12");
+}
+// Company starting
+function analyticalMachine1 {
+	
+}
+function developingGoogleSearch {
+	
+}
+// Google Chrome
+
+// Adblocking in Google Chrome sixty_X_
+function GoogleChrome_RemoveAdBlockingDecision {
+	console.log("People seem to be skipping your ads a lot. These adblockers on the Google Chrome web browser are skipping millions of ads every day. Will you disable adblocking in Google Chrome? (y/n)");
+	var adblockfreeChrome = "y";
+	if adblockfreeChrome == "y" {
+		console.log("You have announced that with Google Chrome [VER] adblockers would begin to get cracked down on. This pissed many users off, however your most loyal fanbase didn't notice or really care. Firefox marketshare increases"; 
+	} else {
+		// You have made a decision that alters the original history of Google. The game will operate differently as you went against canon
+		alert("You have made a decision that alters the original history of Google. The game will operate differently as you went against canon");
+		console.log("You have decided to let ad blockers roam free in Chrome. This lets users skip the extremely annoying ads you refuse to moderate, but it prevents some users from switching to Firefox, and after word got out about your decision, adblocking has increased slightly more than usual");
+		console.log("It was either Chrome marketshare or advertisement marketshare");
+}}
+// ChromeOS
+function developChromeOS1 {
+	console.log("You have a vision for a net operating system known as ChromeOS. It will run on specialized laptops known as Chromebooks. Will you go through witb this? (y/n)");
+	var doChromeOS1 = "y";
+	if doChromeOS1 == "y" {
+		console.log("ChromiumOS is now under development");
+	} else {
+		// You have made a decision that alters the original history of Google. The game will operate differently as you went against canon
+		alert("You have made a decision that alters the original history of Google. The game will operate differently as you went against canon");
+		console.log("You decided to drop the ChromiumOS and ChromeOS projects before development even started");
+}}
+function releaseChromeOS1 { // 2009
+
+}
+function developChromeOS2 {
+	
+}
+// ChromeOS 80 Debian
+function debian10InChromeOS80_Decision {
+	console.log("You are considering switching the base of ChromeOS from Gentoo [confirmation needed] to Debian 10 Buster. Will you go through with the decision? (y/n)");
+	var debian10Chrome80Choice1 = "y";
+	if debian10Chrome80Choice1 = "y" {
+		console.log("You have decided to rebuild ChromeOS on top of Debian. This was meeted with excitement from the Debian community, and ChromeOS can now install .deb packages directly, just like in Debian.");
+	} else {
+		// You have made a decision that alters the original history of Google. The game will operate differently as you went against canon
+		alert("You have made a decision that alters the original history of Google. The game will operate differently as you went against canon");
+		console.log("You have decided to keep ChromeOS on Gentoo. Some parts of the Debian community couldn't care less, some feel like an opportunity was missed. This might affect your marketshare, but it likely won't.");
+}}
+// The Google search engine
+function launchGoogleSearch {
+	
+}
+// The Android operating system
+function developAndroid3HoneyComb {
+	
+}
+function android3HoneyCombPlatformDecision {
+	
+}
+function releaseAndroid3HoneyComb {
+	
+}
+function androidQNamingDecision {
+	console.log("Some Google employees don't want to call Android 10.0 Android Q and are thinking of calling it Android 10. Do you want to continue with the dessert branding? (y/n)");
+	var androidQNamingDecision = "n";
+	if androidQNamingDecision == "y" {
+		alert("You have made a decision that alters the original history of Google. The game will operate differently as you went against canon");
+		console.log("You have decided to call Android 10 Queen Cake, as that was the last minute name. People are happy that the trend is still going.");
+	} else {
+		console.log("You have renamed Android Q to Android 10, ending the dessert naming theme. This upset a lot of users, but it was quickly ignored. Android 10 is now called Android 10, instead of Android 10.0 Queen Cake");
+}}
+/* File version history
+Version 1: January 28th 2020
+Version 2: January 29th 2020 (I didn't have as much time to spend developing this game today.
+Version 3: January 30th 2020
+Version 4: Coming soon
+*/
+//</script>
